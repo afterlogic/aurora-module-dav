8810b2439453cb6d370be0d599cec044ce606ce8
sash04ek <a.ovcharov@gmail.com>
2017-07-17 16:27:38 +0300

    BUG - Creating tables has failed

--- Manager.php
+++ Manager.php
@@ -242,7 +242,7 @@ class Manager extends \Aurora\System\Managers\AbstractManagerWithStorage
 		
 		try
 		{
-			$sFilePath = dirname(__FILE__) . '/storages/db/sql/create.sql';
+			$sFilePath = dirname(__FILE__) . '/Storages/Db/Sql/create.sql';
 			$bResult = $this->oStorage->executeSqlFile($sFilePath);
 		}
 		catch (\Aurora\System\Exceptions\BaseException $oException)
